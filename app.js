4ff2aac1d5057a01c9d8f65bb6d5125a637ca553
Qutub Ahmed <142055092+qutubahmed@users.noreply.github.com>
2024-05-16 15:32:55 +0000

    new

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 alert ("hello world")
+var pak = (a,operator,b) =>{
+if (operator === "+"){
+    alert(a + b)
+}
+else if (operator === "-"){
+    alert(a - b)
+}
+else if(operator === "*"){
+    alert(a * b)
+}
+else {
+    alert("wrong operator")
+}
+}
+pak(4 , +  4);
